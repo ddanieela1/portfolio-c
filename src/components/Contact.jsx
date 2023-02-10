d43703b2a32146e332d499b45eaf8cd98e2eb49f
ddanieela1 <vdaniela.gv@gmail.com>
2023-02-10 17:30:23 -0500

    added contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export const Contact = () => {
+    const contactForm = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+    }
+
+    const [formDetails, setFormDetails] = useState(formInitialDetails);
+
+    return (
+        
+    )
+}
