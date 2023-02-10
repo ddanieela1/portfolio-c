4741c36f924156b4c75315de4ee4f0953f92f6c7
ddanieela1 <vdaniela.gv@gmail.com>
2023-02-09 21:57:46 -0500

    added project module

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -19,7 +19,24 @@ export const Projects = () => {
             <Container>
                 <Row>
                     <Col size={12}>
-                        
+                        <h2>Projects</h2>
+                        <p>Description</p>
+                        <Tab.Container id="projects-tabs" defaultActiveKey="first">
+
+                            <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+
+                                <Nav.Item>
+                                    <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                                </Nav.Item>
+
+                                <Nav.Item>
+                                    <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                                </Nav.Item>
+
+                            </Nav>
+{/* 
+                            <Tab.Content id="slideInUp" className="" */}
+                        </Tab.Container>
                     </Col>
                 </Row>
             </Container>
