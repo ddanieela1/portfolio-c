b362a51f2840e118ce2eacd4f7c91f31046ec593
ddanieela1 <vdaniela.gv@gmail.com>
2023-02-11 12:51:11 -0500

    feat: updated server file for email service

--- server.js
+++ server.js
@@ -8,3 +8,44 @@ const app = express();
 app.use(express.json());
 app.use("/", router);
 app.listen(5000, ()=> console.log('Listening on port 5000'));
+console.log(process.env.EMAIL_USER);
+console.log(process.env.EMAIL_PASS);
+
+
+const sendEmail = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: "vdaniela.gv@gmail.com",
+        pass:""
+    }
+});
+
+sendEmail.verify((error)=> {
+    if(error){
+        console.log(error);
+    }else{
+        console.log("Email available");
+    }
+});
+
+router.post("/contact", (req,res)=> {
+    const name = req.body.firstName + req.body.lastName;
+    const email = req.body.email;
+    const message = req.body.message;
+    const phone = req.body.phone;
+    const mail = {
+        from: name,
+        subject: "Contact from Portfolio",
+        html:`<p>Name: ${name}</p>
+        <p>email: ${email}</p>
+        <p>phone: ${phone}</p>
+        <p> Message: ${message}</p>`,
+    }
+    sendEmail.sendMail(mail,(error)=>{
+        if (error) {
+            res.json(error)
+        }else{
+            res.json({ code:200, status: "Succesfully sent email"})
+        }
+    });
+});
